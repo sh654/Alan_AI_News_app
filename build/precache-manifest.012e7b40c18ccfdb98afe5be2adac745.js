4f0fb908a073cd36487f1fb888c093f6307c5dad
sh654 <shivampdh2105@gmail.com>
2022-04-14 15:21:18 +0530

    message

---
+++ build/precache-manifest.012e7b40c18ccfdb98afe5be2adac745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b22617d9f1cce40b244e42da15607dd",
+    "url": "/Alan_AI_News_app/index.html"
+  },
+  {
+    "revision": "990da54e201987c4d3ff",
+    "url": "/Alan_AI_News_app/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "7ccb9089acef15d5ec5f",
+    "url": "/Alan_AI_News_app/static/js/2.58a8787f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Alan_AI_News_app/static/js/2.58a8787f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "990da54e201987c4d3ff",
+    "url": "/Alan_AI_News_app/static/js/main.8c3a85ca.chunk.js"
+  },
+  {
+    "revision": "d19cf55344f4facfa454",
+    "url": "/Alan_AI_News_app/static/js/runtime-main.7a3bfc98.js"
+  }
+]);
